2187402230a710baba7e424ddde9fba1d7757ba8
Jennifer Oreffo <jennyoreffo@gmail.com>
2024-04-11 05:22:56 -0700

    commit

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+"""A module for encrypting passwords"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password using random salt"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is a hashed password was formed"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
